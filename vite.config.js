cecf0e899fcf553964b2e0f2f703e0640320f4c9
Shiv Arora <passionshiv007@gmail.com>
2025-05-14 11:31:02 +0530

    Production endpoint added

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://ratingapp-7yax.onrender.com',
         changeOrigin: true,
       },
     },
